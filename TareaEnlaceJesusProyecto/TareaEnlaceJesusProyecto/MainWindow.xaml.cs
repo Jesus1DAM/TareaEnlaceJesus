85febbdb480196e7c2f93daf493b0844189bc52c
Jesus1DAM <jesusboni22@gmail.com>
2025-02-04 17:29:11 +0100

    cambios

--- TareaEnlaceJesusProyecto/TareaEnlaceJesusProyecto/MainWindow.xaml.cs
+++ TareaEnlaceJesusProyecto/TareaEnlaceJesusProyecto/MainWindow.xaml.cs
@@ -1,28 +1,121 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Xml.Linq;
 
-namespace TareaEnlaceJesusProyecto
+namespace BindingExample
 {
-    /// <summary>
-    /// Lógica de interacción para MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        private string _nombre;
+        private string _telefono;
+        private bool _puedeGuardar;
+
+        public string Nombre
+        {
+            get => _nombre;
+            set
+            {
+                _nombre = value;
+                OnPropertyChanged(nameof(Nombre));
+                VerificarHabilitacion();
+            }
+        }
+
+        public string Telefono
+        {
+            get => _telefono;
+            set
+            {
+                _telefono = value;
+                OnPropertyChanged(nameof(Telefono));
+                VerificarHabilitacion();
+            }
+        }
+
+        public bool PuedeGuardar
+        {
+            get => _puedeGuardar;
+            set
+            {
+                _puedeGuardar = value;
+                OnPropertyChanged(nameof(PuedeGuardar));
+            }
+        }
+
+        public ICommand GuardarCommand { get; }
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+            GuardarCommand = new RelayCommand(Guardar, () => PuedeGuardar);
+        }
+
+        private void VerificarHabilitacion()
+        {
+            PuedeGuardar = !string.IsNullOrWhiteSpace(Nombre) && !string.IsNullOrWhiteSpace(Telefono);
+        }
+
+        private void Guardar()
+        {
+            try
+            {
+                string rutaArchivo = "datos.xml";
+                XDocument xmlDoc;
+
+                if (File.Exists(rutaArchivo))
+                {
+                    xmlDoc = XDocument.Load(rutaArchivo);
+                    xmlDoc.Root.Add(new XElement("Contacto",
+                        new XElement("Nombre", Nombre),
+                        new XElement("Telefono", Telefono)));
+                }
+                else
+                {
+                    xmlDoc = new XDocument(
+                        new XElement("Contactos",
+                            new XElement("Contacto",
+                                new XElement("Nombre", Nombre),
+                                new XElement("Telefono", Telefono)))
+                    );
+                }
+
+                xmlDoc.Save(rutaArchivo);
+                MessageBox.Show("Datos guardados en datos.xml", "Éxito", MessageBoxButton.OK, MessageBoxImage.Information);
+                Nombre = string.Empty;
+                Telefono = string.Empty;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al guardar: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    public class RelayCommand : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
+
+        public RelayCommand(Action execute, Func<bool> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter) => _canExecute == null || _canExecute();
+        public void Execute(object parameter) => _execute();
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
         }
     }
 }
